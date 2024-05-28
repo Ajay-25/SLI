5cee91305083ca8496297c552ef6d933d3fe480d
Ajay Kumar <ajay.kumar@sprinklr.com>
2024-05-29 00:39:43 +0530

    Add logon in video

--- app/ui/home/videoContainer/VideoContainer.tsx
+++ app/ui/home/videoContainer/VideoContainer.tsx
@@ -1,9 +1,17 @@
 //components
+import Image from 'next/image';
 import { Video } from '../video';
 
 const VideoContainer = () => {
   return (
-    <div className="h-[42rem] w-[70rem] border-8 border-sos-primary-gold">
+    <div className="relative h-[42rem] w-[70rem] border-8 border-sos-primary-gold">
+      <Image
+        src="/home/home-logo.webp"
+        width={94}
+        height={94}
+        className="absolute left-1/2 block -translate-x-1/2 -translate-y-2/3 transform shadow-lg"
+        alt="Service Leadership Institute text with logo"
+      />
       <Video />
     </div>
   );
