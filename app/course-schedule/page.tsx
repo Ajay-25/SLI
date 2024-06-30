2576640d087bb48553a01882e6c9ff99954d5526
Ajay Kumar <ajay.kumar@sprinklr.com>
2024-07-01 00:46:53 +0530

    Integrate course schedule Api

--- app/course-schedule/page.tsx
+++ app/course-schedule/page.tsx
@@ -1,65 +1,24 @@
 //components
 import { SectionSeparator } from '@/app/ui/home/SectionSeparator';
 
+//utils
+import { adaptCourses } from '@/app/ui/courseSchedule/utils';
+
 //types
 import { ReactElement } from 'react';
 import { Metadata } from 'next';
+import { Config, CourseSchedule } from '@/app/ui/courseSchedule/types';
 
-type Course = {
-  name: string;
-  trainingDate: string;
-  startTime: string;
-  endTime: string;
-  timezone: string;
-  venue: string;
-  facilitators: string[];
-  language: string;
-  seats?: number;
-  totalSeats?: number;
-};
-
-type Config = Array<{ month: string; courses: Array<Course> }>;
-
-const config: Config = [
-  {
-    month: 'June',
-    courses: [
-      {
-        name: 'Loving communication',
-        trainingDate: 'Jun 2nd ',
-        startTime: '10:30am',
-        endTime: '1:30pm',
-        timezone: 'CDT',
-        venue: 'Lisle Center',
-        facilitators: ['Kait', 'Vicki Lumpkin'],
-        language: 'English',
-      },
-    ],
-  },
-  {
-    month: 'July',
-    courses: [
-      {
-        name: 'Meeting Management',
-        trainingDate: 'Jul 14',
-        startTime: '10:00am',
-        endTime: '1:00pm',
-        timezone: 'CDT',
-        venue: 'Virtual',
-        facilitators: ['Tom', 'Karla'],
-        language: 'English',
-      },
-    ],
-  },
-];
-
-const CoursesList = (): ReactElement[] => {
+const CoursesList = ({ config }: { config: Config }): ReactElement[] => {
   return config.map(({ month, courses }) => (
     <div key={month} className="flex flex-col gap-6 font-medium">
       <h2 className="text-42">{month}</h2>
       <div className="grid grid-cols-2 gap-12">
         {courses.map((course) => (
-          <div key={course.name} className="flex flex-col gap-3">
+          <div
+            key={course.name}
+            className="flex flex-col justify-between gap-3"
+          >
             <h3 className="text-24">{course.name}</h3>
             <div className="flex flex-col gap-3 text-16">
               <span>{`${course.trainingDate}, ${course.startTime}-${course.endTime}, ${course.timezone}`}</span>
@@ -80,14 +39,24 @@ const CoursesList = (): ReactElement[] => {
   ));
 };
 
-export default function Page() {
+async function getServerSideProps(): Promise<CourseSchedule[]> {
+  const res = await fetch(
+    'https://scd.sos.org/api/SLIInfo/getSLIModuleSchedule',
+  );
+  return await res.json();
+}
+
+export default async function Page() {
+  const courses = await getServerSideProps();
+  const coursesConfig = adaptCourses(courses);
+
   return (
     <article className="flex flex-col gap-6 px-[14rem] pb-[8rem] pt-[6rem] text-sos-primary-blue">
       <h1 className="self-start text-center text-42 font-medium">
         Course Schedule
       </h1>
       <SectionSeparator />
-      <CoursesList />
+      <CoursesList config={coursesConfig} />
     </article>
   );
 }
---
+++ app/ui/courseSchedule/utils.ts
@@ -0,0 +1,120 @@
+import { CourseSchedule, Config, AdaptedCourse } from './types';
+
+type Iteratee<T> = ((item: T) => string | number) | keyof T;
+
+function groupBy<T>(array: T[], iteratee: Iteratee<T>): Record<string, T[]> {
+  return array.reduce((result: Record<string, T[]>, value: T) => {
+    let key: string | number;
+
+    if (typeof iteratee === 'function') {
+      key = iteratee(value);
+    } else if (typeof iteratee === 'string') {
+      key = value[iteratee] as unknown as string | number;
+    } else {
+      throw new Error('Iteratee must be a function or string');
+    }
+
+    if (!result[key]) {
+      result[key] = [];
+    }
+
+    result[key].push(value);
+
+    return result;
+  }, {});
+}
+
+function formatDate(date: Date): string {
+  const options: Intl.DateTimeFormatOptions = {
+    month: 'short',
+    day: 'numeric',
+  };
+  return date.toLocaleDateString('en-US', options);
+}
+
+function formatTime(date: Date): string {
+  const hours = date.getHours();
+  const minutes = date.getMinutes();
+  const ampm = hours >= 12 ? 'pm' : 'am';
+  const formattedHours = hours % 12 || 12;
+  const formattedMinutes = minutes.toString().padStart(2, '0');
+  return `${formattedHours}:${formattedMinutes}${ampm}`;
+}
+
+function formatEvent(
+  startDate: string,
+  endDate: string,
+  timezoneAbbr: string,
+): string {
+  const start = new Date(startDate);
+  const end = new Date(endDate);
+
+  // Format the start date, start time, and end time
+  const formattedStartDate = formatDate(start);
+  const formattedStartTime = formatTime(start);
+  const formattedEndTime = formatTime(end);
+
+  return `${formattedStartDate}, ${formattedStartTime}-${formattedEndTime}, ${timezoneAbbr}`;
+}
+
+function getDateConfig(dateString: string): {
+  month: number;
+  day: number;
+  year: number;
+} {
+  const date = new Date(dateString);
+
+  if (isNaN(date.getTime())) {
+    throw new Error('Invalid date string');
+  }
+
+  const year = date.getFullYear();
+  const month = date.getMonth() + 1;
+  const day = date.getDate();
+
+  return { month, day, year };
+}
+
+export const adaptCourses = (courses: CourseSchedule[]): Config => {
+  const transformedCourses = courses.reduce<
+    Array<AdaptedCourse & { month: string }>
+  >((acc, course) => {
+    if (course.Module) {
+      acc.push({
+        month: new Date(course.startTime).toLocaleString('en-US', {
+          month: 'long',
+        }),
+        name: course.Module.name,
+        trainingDate: formatDate(new Date(course.startTime)), // use formatEvent
+        startTime: formatTime(new Date(course.startTime)),
+        endTime: formatTime(new Date(course.endTime)),
+        timezone: course.trainingTimeZone,
+        venue: course.venue,
+        facilitators: [
+          course.Module.facilitator,
+          course.Module.Facilitators,
+        ].filter(Boolean),
+        language: course.language,
+        seats: course.seats,
+        totalSeats: course.confirmed + course.applied,
+      });
+    }
+
+    return acc;
+  }, []);
+
+  const groupedCourses = groupBy(transformedCourses, (course) => course.month);
+
+  const config: Config = Object.entries(groupedCourses)
+    .map(([month, courses]) => ({
+      month,
+      courses,
+    }))
+    .sort((a, b) => {
+      const monthA = new Date(`01 ${a.month}`).getMonth();
+      const monthB = new Date(`01 ${b.month}`).getMonth();
+      return monthA - monthB;
+    });
+
+  return config;
+};
