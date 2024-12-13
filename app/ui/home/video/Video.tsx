3d9e09990126c97611f723ab8694f85f294d3409
Ajay Kumar <ajay.kumar@sprinklr.com>
2024-12-13 23:08:04 +0530

    Revert "update youtube video"
    This reverts commit d156aeeaf31ced5f5bc99e58dde52a3c69206580.

--- app/ui/home/video/Video.tsx
+++ app/ui/home/video/Video.tsx
@@ -6,7 +6,7 @@ const Video = () => {
       <iframe
         width="100%"
         height="100%"
-        src="https://www.youtube.com/embed/rB_P3ZPosEY?si=zELvQz_py32_KJHO&rel=0"
+        src="https://www.youtube.com/embed/EZRg66nPLN4?si=xefRbrEaHOanyCe1&rel=0"
         title="The Service Leadership Institute"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
         referrerPolicy="strict-origin-when-cross-origin"
