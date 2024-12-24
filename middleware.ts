c084f5f6095ffe6e7be9ab6fb22f73e9af2e47e3
Ajay Kumar <ajkumar253@gmail.com>
2024-12-24 22:44:01 +0530

    Restrict India and Middle east users to access the website
    # Conflicts:
    #	middleware.ts

---
+++ middleware.ts
@@ -0,0 +1,44 @@
+// middleware.js
+import { NextResponse, NextRequest } from 'next/server';
+
+const MIDDLE_EAST_COUNTRIES = [
+  'BH',
+  'CY',
+  'EG',
+  'IR',
+  'IQ',
+  'IL',
+  'JO',
+  'KW',
+  'LB',
+  'OM',
+  'PS',
+  'QA',
+  'SA',
+  'SY',
+  'TR',
+  'AE',
+  'YE',
+];
+
+const RESTRICTED_COUNTRIES = ['IN', ...MIDDLE_EAST_COUNTRIES];
+
+export function middleware(request: NextRequest) {
+  const country = request.geo?.country || 'US';
+  const url = request.nextUrl.clone();
+  console.log('country', country);
+
+  // Redirect based on country
+  if (
+    RESTRICTED_COUNTRIES.includes(country) &&
+    url.pathname?.includes('/error')
+  ) {
+    url.pathname = '/error';
+    return NextResponse.redirect(url);
+  }
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: '/:path*', // Match all paths
+};
