471ded140450355d2733ecb70666821e3ae9cf54
Ajay Kumar <ajkumar253@gmail.com>
2025-01-12 14:47:10 +0530

    Fix MyProfile page syntax

--- app/my-profile/page.tsx
+++ app/my-profile/page.tsx
@@ -1,7 +1,6 @@
 import { cookies } from 'next/headers';
 import Image from 'next/image';
 
-
 import { SectionSeparator } from '@/app/ui/home/SectionSeparator';
 
 //types
@@ -11,43 +10,52 @@ import { ReactNode } from 'react';
 import { Metadata } from 'next';
 import { ProfileInfo } from '@/app/ui/myProfile/types';
 
-const Info = ({profileInfo, children}: {profileInfo: ProfileInfo, 
-  children: ReactNode;}) => {
-    return (
-        <div className="flex gap-4 p-4 lg:gap-8 lg:p-12">
-          <Image
-            src={'https://scd.sos.org'+profileInfo.imagePath}
-            width={180}
-            height={180}
-            alt=''
-            className="hidden self-start lg:block"
-          />
-        <Image
-            src={'https://scd.sos.org'+profileInfo.imagePath}
-            width={60}
-            height={60}
-            alt=''
-            className="self-start lg:hidden"
-        />
-          <div className="text-16 text-sos-primary-blue lg:text-20 text-justify px-10 lg:px-20">{children}</div>
-        </div>
-      );
-}
+const Info = ({
+  profileInfo,
+  children,
+}: {
+  profileInfo: ProfileInfo;
+  children: ReactNode;
+}) => {
+  return (
+    <div className="flex gap-4 p-4 lg:gap-8 lg:p-12">
+      <Image
+        src={'https://scd.sos.org' + profileInfo.imagePath}
+        width={180}
+        height={180}
+        alt=""
+        className="hidden self-start lg:block"
+      />
+      <Image
+        src={'https://scd.sos.org' + profileInfo.imagePath}
+        width={60}
+        height={60}
+        alt=""
+        className="self-start lg:hidden"
+      />
+      <div className="px-10 text-justify text-16 text-sos-primary-blue lg:px-20 lg:text-20">
+        {children}
+      </div>
+    </div>
+  );
+};
 
-const  ToDos = () => {
-    return (
-        <article className="flex flex-col gap-2 px-[2.2rem] pb-[4rem] pt-[2rem] text-sos-secondary-blue lg:gap-6 lg:px-[14rem] lg:pb-[8rem] lg:pt-[6rem]">
-            <h1 className="self-start text-center text-32 font-medium lg:text-38">
-                To Do's
-            </h1>
-        </article>
+const ToDos = () => {
+  return (
+    <article className="flex flex-col gap-2 px-[2.2rem] pb-[4rem] pt-[2rem] text-sos-secondary-blue lg:gap-6 lg:px-[14rem] lg:pb-[8rem] lg:pt-[6rem]">
+      <h1 className="lg:text-38 self-start text-center text-32 font-medium">
+        To Do&#39;s
+      </h1>
+    </article>
+  );
+};
 
-    );
-}
-
-async function getServerSideProps(userId: string | undefined): Promise<ProfileInfo> {
+async function getServerSideProps(
+  userId: string | undefined,
+): Promise<ProfileInfo> {
   const res = await fetch(
-    'https://scd.sos.org/api/CommonServicesController/getSevadarIDFromDnnUserID?dnnUserID='+userId,
+    'https://scd.sos.org/api/CommonServicesController/getSevadarIDFromDnnUserID?dnnUserID=' +
+      userId,
     {
       cache: 'no-store',
       method: 'GET',
@@ -60,29 +68,29 @@ async function getServerSideProps(userId: string | undefined): Promise<ProfileIn
 }
 
 export default async function Page() {
-    const cookiesList = cookies();
-    const userId = cookiesList.get('authToken');
-    const profileInfo = await getServerSideProps(userId?.value);
-  
-    return (
-      <article className="flex flex-col gap-2 px-[2.2rem] pb-[4rem] pt-[2rem] text-sos-primary-blue lg:gap-6 lg:px-[14rem] lg:pb-[8rem] lg:pt-[6rem]">
-        <h1 className="self-start text-center text-32 font-medium lg:text-42">
-          My Profile
-        </h1>
-        <SectionSeparator />
-        <div className="flex flex-col lg:mx-[14.2rem]">
-            <Info profileInfo={profileInfo}>
-                <div>
-                    <div>{profileInfo.fullName}</div>
-                    <div>{profileInfo.email}</div>
-                </div>
-            </Info>
-        </div>
-        <ToDos></ToDos>
-      </article>
-    );
-  }
+  const cookiesList = await cookies();
+  const userId = cookiesList.get('authToken');
+  const profileInfo = await getServerSideProps(userId?.value);
+
+  return (
+    <article className="flex flex-col gap-2 px-[2.2rem] pb-[4rem] pt-[2rem] text-sos-primary-blue lg:gap-6 lg:px-[14rem] lg:pb-[8rem] lg:pt-[6rem]">
+      <h1 className="self-start text-center text-32 font-medium lg:text-42">
+        My Profile
+      </h1>
+      <SectionSeparator />
+      <div className="flex flex-col lg:mx-[14.2rem]">
+        <Info profileInfo={profileInfo}>
+          <div>
+            <div>{profileInfo.fullName}</div>
+            <div>{profileInfo.email}</div>
+          </div>
+        </Info>
+      </div>
+      <ToDos></ToDos>
+    </article>
+  );
+}
 
 export const metadata: Metadata = {
-    title: 'My Profile',
-  };
+  title: 'My Profile',
+};
