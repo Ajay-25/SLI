427bfeecb7691718193c29ab2a27b03703586233
Ajay Kumar <ajkumar253@gmail.com>
2025-01-12 14:12:57 +0530

    Upgrade to next 15 and react 19

---
+++ app/ui/googleTranslate/index.ts
@@ -0,0 +1,1 @@
+export { GoogleTranslate } from './GoogleTranslate';
---
+++ app/ui/googleTranslate/languages.ts
@@ -0,0 +1,51 @@
+// src/languages.ts
+
+type Language = {
+  label: string;
+  value: string;
+};
+
+export const LANGUAGES: Language[] = [
+  { label: 'Afrikaans', value: '/auto/af' },
+  { label: 'Albanian', value: '/auto/sq' },
+  { label: 'Arabic', value: '/auto/ar' },
+  { label: 'Armenian', value: '/auto/hy' },
+  { label: 'Azerbaijani', value: '/auto/az' },
+  { label: 'Basque', value: '/auto/eu' },
+  { label: 'Belarusian', value: '/auto/be' },
+  { label: 'Bengali', value: '/auto/bn' },
+  { label: 'Bosnian', value: '/auto/bs' },
+  { label: 'Bulgarian', value: '/auto/bg' },
+  { label: 'Catalan', value: '/auto/ca' },
+  { label: 'Chinese (Simplified)', value: '/auto/zh-CN' },
+  { label: 'Chinese (Traditional)', value: '/auto/zh-TW' },
+  { label: 'Croatian', value: '/auto/hr' },
+  { label: 'Czech', value: '/auto/cs' },
+  { label: 'Danish', value: '/auto/da' },
+  { label: 'Dutch', value: '/auto/nl' },
+  { label: 'English', value: '/auto/en' },
+  { label: 'Estonian', value: '/auto/et' },
+  { label: 'Finnish', value: '/auto/fi' },
+  { label: 'French', value: '/auto/fr' },
+  { label: 'Galician', value: '/auto/gl' },
+  { label: 'Georgian', value: '/auto/ka' },
+  { label: 'German', value: '/auto/de' },
+  { label: 'Greek', value: '/auto/el' },
+  { label: 'Gujarati', value: '/auto/gu' },
+  { label: 'Haitian Creole', value: '/auto/ht' },
+  { label: 'Hebrew', value: '/auto/he' },
+  { label: 'Hindi', value: '/auto/hi' },
+  { label: 'Hungarian', value: '/auto/hu' },
+  { label: 'Icelandic', value: '/auto/is' },
+  { label: 'Indonesian', value: '/auto/id' },
+  { label: 'Irish', value: '/auto/ga' },
+  { label: 'Italian', value: '/auto/it' },
+  { label: 'Japanese', value: '/auto/ja' },
+  { label: 'Kazakh', value: '/auto/kk' },
+  { label: 'Korean', value: '/auto/ko' },
+  { label: 'Kurdish (Kurmanji)', value: '/auto/ku' },
+  // Add more languages as needed...
+
+  //spanish language
+  { label: 'Spanish', value: '/auto/es' },
+];
